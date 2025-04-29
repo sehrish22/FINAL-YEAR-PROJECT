87d1ab1f59588966170b76551ea783e770b20a28
sehrish22 <sp21-bcs-089@cuilahore.edu.pk>
2025-04-29 10:59:31 +0500

    .

--- extraworkcurson/middlewares/checkBuyerAuth.js
+++
@@ -1,8 +0,0 @@
-function checkBuyerAuth(req, res, next) {
-    if (!req.session || !req.session.user || req.session.user.role !== "buyer") {
-      return res.redirect('/login');
-    }
-    next();
-  }
-  module.exports = checkBuyerAuth;
-  
